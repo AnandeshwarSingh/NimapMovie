62f17f4239a2ec056432e35e57c0e8ad4c321432
Anandeshwar Singh <137038657+AnandeshwarSingh@users.noreply.github.com>
2025-04-14 12:21:29 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import NavBar from './Componenet/NavBar'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+function App() {
+ 
+
+  return (
+    <>
+      <div className="masterContainer text-white">
+        <NavBar/>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Componenet/MovieDetail.jsx
@@ -0,0 +1,121 @@
+import React, { useState , useEffect } from "react";
+
+function MovieDetail(props) 
+{
+    const [movie , setMovie] = useState(null);
+    const [load , setLoad] = useState(true);
+    const [error , setError] = useState(null);
+    const [cast , setCast] = useState(null);
+
+    const Api_key = "c45a857c193f6302f2b5061c3b85e743";
+      
+    useEffect(() => 
+        {
+        if (props.getMovie) 
+        {
+            const movie_id = props.getMovie.id;
+            const Api_url = `https://api.themoviedb.org/3/movie/${movie_id}?api_key=${Api_key}&language=en-US`;
+            const Cast_url = `https://api.themoviedb.org/3/movie/${movie_id}/credits?api_key=${Api_key}&language=en-US`;
+    
+            setLoad(true);
+            Promise.all([
+                fetch(Api_url).then((response) => 
+                {
+                    if (!response.ok) 
+                    {
+                        throw new Error("Failed to fetch movie details");
+                    }
+                    return response.json();
+                }),
+                fetch(Cast_url).then((response) => 
+                {
+                    if (!response.ok) 
+                    {
+                        throw new Error("Failed to fetch cast details");
+                    }
+                    return response.json();
+                }),
+            ])
+            .then(([movieData, castData]) => 
+            {
+                setMovie(movieData);
+                setCast(castData); 
+                setLoad(false); 
+            })
+            .catch((error) => 
+            {
+                setError(error.message);
+                setLoad(false); 
+            });
+        } 
+        else 
+        {
+            setLoad(false); 
+        }
+    }, [props.getMovie]);
+    
+    if (load) return <h1>Loading...</h1>;
+    if (error) return <h1>Error: {error}</h1>;
+    if (!movie) return <h1>No movie data available. Please select a movie.</h1>;
+    return (
+        <>
+            <div className="container  mt-5 p-0">
+                {props.getMovie ? (
+                    <>
+                        <div className="banner row m-0 p-2">
+                            <div className="left m-0 p-0 row col-md-6 d-flex flex-wrap">
+                                {/* ===================================================== */}
+                                <div className="l-box col-3  p-0">
+                                    <div className="img-b w-100" ><img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title}className="img-fluid rounded shadow"/></div>
+                                </div>
+                                {/* ===================================================== */}
+                                <div className="r-box p-2 col-8 ">
+                                    <h1 className="my-2 p-0 h3 b-col" >{movie.title}</h1>
+                                    <h1 className="h5 text-primary" style={{color:"#f9ab00"}} >Rating : {movie.vote_average}</h1>
+                                    <div className="box d-flex mt-3">
+                                        <p className="time rounded-2 p-1 my-auto">{movie.runtime} min</p> 
+                                        <div className="box mx-2 pt-1 text-info-emphasis">{movie.genres.map((element , index)=><span key={index} >{element.name + "  "}</span >)}</div>
+                                    </div>
+                                    <p className="mt-3 l-col"><strong>Release Date:</strong> {movie.release_date}</p>
+                                </div>
+                                {/* ===================================================== */}
+                                <div className="overview  col-11 m-0">
+                                    <h3 className="h4 m-0 b-col mt-1">Overview:</h3>
+                                    <p className="m-0 py-2 lh-2 l-col">{movie.overview}</p>
+                                </div>
+                            </div>
+                            <div className="right col-md-6 boder p-0">
+                                <img className="img-banner shadow w-100" src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`} alt="" />
+                            </div>
+                        </div>
+                    </>
+                ) : 
+                (
+                    <h1>No movie data available. Please select a movie....</h1>
+                )}
+            </div>
+            <div className="container-fluid cast my-4" >
+                    <h2 className="b-col d-cast text-center h5 p-1 rounded-5 o">Cast</h2>
+                    <div className="row text-center">
+                        {cast.cast.map((element , index)=>
+                        <div className="col-2 m-auto w-auto my-2" key={element.id}>
+                        <div className="card card-d">
+                            <img
+                                src={`https://image.tmdb.org/t/p/w500${element.profile_path}`}
+                                alt={element.name}
+                                className="card-img-top"
+                            />
+                            <div className="card-body">
+                                <h5 className="card-title m-0">{element.name}</h5>
+                                <p className="card-text">Character : {element.character}</p>
+                            </div>
+                        </div>
+                        </div>
+                        )}
+                    </div>
+                </div>
+        </>
+    );
+}
+
+export default MovieDetail;
